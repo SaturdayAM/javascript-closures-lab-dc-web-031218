70e740fb76c450e68eb479f26448056427d80109
SaturdayAM <jwl62@georgetown.edu>
2018-04-26 09:32:13 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 const app = "I don't do much."
+
+function bumpCounter(){
+	let counter = 0;
+	return{
+		getBumps:() => counter,
+
+		addBump: () => {++counter}
+	};
+};
+
+function createAnimal(){
+	function sharkCreator(deadlyDevice){
+		return{
+			'animalType': 'Shark',
+			'deadlyDevice': deadlyDevice
+		};
+	};
+	return sharkCreator;
+}
+
+let sharkCreator = createAnimal();
+let sharkWithFrickinLaserbeam = sharkCreator('Laserbeam');
+let sharkWithFrickinCannon = sharkCreator('Cannon');
